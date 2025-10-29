bb28e85282b0c9b37e87dc71fbe42d8cd16de40c
Ivan Orisakwe <Ivan.Orisakwe@kukkula.local>
2025-10-29 14:20:51 +0200

    made database conncection, with users as well

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,16 @@
-let mysql = require('mysql');
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+
 }).promise()
 
+console.log('process.env.DB_HOST:', process.env.DB_HOST);
 export default pool;
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,11 @@
 import express from "express";
+import { getUsers } from "../db/users.js";
 
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('user output');
+router.get('/', async function(req, res, next) {
+  res.json(await getUsers());
 });
 
 export default router;
