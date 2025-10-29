c834a36c7c3f88c1747c09a73d998c76debf70d2
Ivan Orisakwe <Ivan.Orisakwe@kukkula.local>
2025-10-29 13:50:18 +0200

    db and added code to the db.js file

---
+++ server/db/db.js
@@ -0,0 +1,10 @@
+let mysql = require('mysql');
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+}).promise()
+
+export default pool;
+
