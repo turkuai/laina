e37bc78fdca606ab85c653ad50afe90e0c805396
Ivan Orisakwe <ivanorisakwe52@gmail.com>
2026-01-22 14:20:28 +0200

    prodcuts for desktop now shows for students

--- client-web/src/components/Admin.js
+++ client-web/src/components/Admin.js
@@ -283,7 +283,7 @@ export default function Admin({ productsData }) {
             />
           )}
 
-          {activeTab === 'products' && currentUser?.role === 'admin' && (
+          {activeTab === 'products' && (
             <ProductsTab
               currentUser={currentUser}
               productsData={productsData}
