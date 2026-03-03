663cb7808e080f412b63c7549faf3acaf93628d5
Ivan Orisakwe <Ivan.Orisakwe@kukkula.local>
2026-03-03 13:56:06 +0200

    infinite scroll page

--- client-vite/src/components/DataSetView.js
+++ client-vite/src/components/DataSetView.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {useState} from "react";
 
-export default function DataSetView({ data, render: Render, loadPage }) {
+export default function DataSetView({ render: Render, loadPage }) {
 
   const [page, setPage] = useState(1);
-  const [items, setItems] = useState(data);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+
+    console.log("Loading page", page);
+
+    loadPage(page).then((pageItems) => {
+      setItems((prev) => [...prev, ...pageItems]);
+    });
+  }, [page]);
   const handleScroll = (e) => {
     const el = e.currentTarget;
     if (el.scrollTop + el.clientHeight >= el.scrollHeight - 1) {
       console.log("Reached bottom of div");
       setPage((prev) => prev + 1);
-      loadPage(page + 1).then((pageItems) => {
-        setItems((prev) => [...prev, ...pageItems]);
-      });
     }
   };
   return (
